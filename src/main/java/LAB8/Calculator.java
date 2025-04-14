ef12170a811b30bb23d958609ac34265aad3fbb9
Dajtobiez <doanquocddatj@gmail.com>
2025-04-14 13:40:14 +0700

    update workflows

--- src/main/java/LAB8/Calculator.java
+++ src/main/java/LAB8/Calculator.java
@@ -2,9 +2,9 @@ package LAB8;
 
 public class Calculator implements ICalculator{
 	public Integer sum(Integer a, Integer b) {
-		return a+b;
+		return a/b;
 	}
 	public Integer sub(Integer a, Integer b) {
-		return a-b;
+		return 0;
 	}
 }
--- src/main/java/test/TestCalculator.java
+++ src/main/java/test/TestCalculator.java
@@ -2,18 +2,18 @@ package test;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import LAB8.Calculator;
 
 public class TestCalculator {
-	Calculator calculator = new Calculator();
-	@Test
-	public void testSum() {
-		Assert.assertEquals(calculator.sum(2, 3), 5);
-	}
-	
-	@Test
-	public void testSub() {
-		Assert.assertEquals(calculator.sum(2, 3), -1);
-	}
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        Assert.assertEquals(calculator.sum(2, 3), 5);
+    }
+
+    @Test
+    public void testSub() {
+        Assert.assertEquals(calculator.sub(2, 3), -1);
+    }
 }
