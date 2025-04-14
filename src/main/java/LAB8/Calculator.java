6049a8a935ec7dd1d9cc9e1c5f14c6a73a3692ee
Dajtobiez <doanquocddatj@gmail.com>
2025-04-14 14:20:32 +0700

    update fail

--- src/main/java/LAB8/Calculator.java
+++ src/main/java/LAB8/Calculator.java
@@ -2,7 +2,7 @@ package LAB8;
 
 public class Calculator implements ICalculator{
 	public Integer sum(Integer a, Integer b) {
-		return a/b;
+		return 0;
 	}
 	public Integer sub(Integer a, Integer b) {
 		return 0;
